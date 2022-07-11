1d540f329e017848f5f9efd121f5335ae826c8f1
Zain <syedzainnaqvi3324@gmail.com>
2022-07-11 16:27:31 +0500

    Errors fixes

--- lib/edit/components/body.dart
+++ lib/edit/components/body.dart
@@ -1,7 +1,5 @@
-import 'dart:html';
-
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
